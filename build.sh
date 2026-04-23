25fb2dd75f6cc03f10eb7883c57c0d95288d671e
Mohd Siraj <Mohd.Siraj@rmsi.com>
2026-04-23 21:38:47 +0530

    add db

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 export DJANGO_SECRET_KEY="${DJANGO_SECRET_KEY:-render-build-only-secret-key-change-at-runtime-1234567890}"
 python manage.py migrate --noinput
+if [ -f data.json ]; then
+  python manage.py loaddata data.json
+fi
 python manage.py collectstatic --noinput
